34abe1be781a27269e42794f82769a6874b8cb0e
palmbeachjeff <palmbeachjeff@gmail.com>
2015-02-18 15:59:08 -0500

    added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218140305) do
+ActiveRecord::Schema.define(version: 20150218204513) do
 
   create_table "messages", force: true do |t|
     t.string   "message"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20150218140305) do
     t.boolean  "resolved"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
