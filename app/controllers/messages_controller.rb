5607a18053b4cf5b80956526f9633515dc8ec056
Prelang Builder <builder@prelang.com>
2015-02-18 14:03:08 +0000

    Setting the User of the model on #create to the currently signed in user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,6 +29,7 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
+    @message.user = current_user
 
     respond_to do |format|
       if @message.save
